636e1553709b5d2336338935e4e4ff807a2195ae
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2023-03-19 21:39:19 +0100

    Special CD & CWD features, last exit code
    	modified:   ruby_comline.rb

--- ruby_comline.rb
+++ ruby_comline.rb
@@ -9,8 +9,45 @@ require 'readline'
 $x = 55
 x = 5
 
+$prompt = '#{$last_exit_code.to_s.rjust 3} stdin> '
+$last_exit_code = 0
+
+$cwd_history = [Dir.pwd]
+$cwd_max_length = 5
+$home_dir_aliases = ['~', '']
+
+def cwd dir
+  # special cases
+  if $home_dir_aliases.include? dir.strip
+    directory = Dir.home
+  elsif dir.strip == '-'
+    directory = $cwd_history[-1]
+  else
+    directory = dir
+  end
+
+  #
+  begin
+    prev_dir = Dir.pwd
+    Dir.chdir directory
+
+    # update cwd history
+    # make place in history, if it's at max
+    if $cwd_history.count >= $cwd_max_length
+      $cwd_history.shift
+    end
+    # push new element, if it is new
+    if prev_dir != $cwd_history[-1]
+      $cwd_history.push prev_dir
+    end
+
+  rescue Errno::ENOENT => error
+    puts error.message
+  end
+end
+
 def console (at_binding = nil)
-  while usr_command = Readline.readline('stdin> ', true)
+  while usr_command = Readline.readline(eval('"' + $prompt + '"'), true)
 
     # special control cases
     if usr_command.strip.empty?
@@ -23,7 +60,8 @@ def console (at_binding = nil)
       next
 
     elsif usr_command.strip[...2] == 'cd' # turn it into z?
-      Dir.chdir usr_command.strip[2...].strip
+      directory = usr_command.strip[2...].strip
+      cwd directory
       next
     end
 
@@ -36,7 +74,8 @@ def console (at_binding = nil)
 
     begin
       stdin, stdout, stderr, wait_thr = Open3.popen3(usr_command)
-      puts "#{stdout.read} #{stderr.read} #{wait_thr.value.exitstatus}"
+      $last_exit_code = wait_thr.value.exitstatus
+      puts "#{stdout.read} #{stderr.read}"
 
     rescue Errno::ENOENT => error
       # command not found
